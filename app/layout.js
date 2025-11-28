5c182a1f73990ea57f2e8877655c6793b896e8e8
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-28 22:57:57 +0530

    Update the page.js to import Navbar

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,12 @@ import "./globals.css";
 
 const outfit = Outfit({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"]
+  weight: ["400", "500", "600", "700"],
 });
 
 const ovo = Ovo({
   subsets: ["latin"],
-  weight: ["400"]
+  weight: ["400"],
 });
 
 export const metadata = {
