97a66d01a264eda437cd40a0af8ea9885f1f0e53
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-30 02:27:23 +0530

    Align the ul and use custom font in className

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,21 +5,18 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <>
-        <nav>
-            <a href="">
-                <Image src={assets.logo} className='w-28 cursor-pointer mr-14' />
             </a>
 
-            <ul>
-                <li><a href="#top">Home</a></li>
-                <li><a href="#about">About me</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#work">My Work</a></li>
-                <li><a href="#contact">Contact me</a></li>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+                <li><a className="font-Ovo" href="#top">Home</a></li>
+                <li><a className="font-Ovo" href="#about">About me</a></li>
+                <li><a className="font-Ovo" href="#services">Services</a></li>
+                <li><a className="font-Ovo" href="#work">My Work</a></li>
+                <li><a className="font-Ovo" href="#contact">Contact me</a></li>
             </ul>
 
             <div>
-                <a href='#contact'>Contact <Image src={assets.arrow_icon} className='w-3'/></a>
+                <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} className='w-3' alt=""/></a>
             </div>
         </nav>
     </>
