fab3317d5180a2ff8123b974c37fdd6047a9d446
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-30 02:27:41 +0530

    Use custom image in ul bg

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,6 +5,12 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+    </div>
+        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+            <a href="#top">
+                <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="" />
             </a>
 
             <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
