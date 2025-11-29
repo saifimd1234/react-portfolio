00e79010435385bb5a2439750d8f90b3ba1aa15c
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-30 03:59:52 +0530

    Update the page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+'use client'
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 }
